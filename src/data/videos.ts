5adb084de3a9ad19da0b48866cc1ebbac078a93e
HPHuerta <hhernanp@gmail.com>
2026-05-05 00:00:46 -0400

    feat: add ContentShowCase component to display family lifestyle videos

---
+++ src/data/videos.ts
@@ -0,0 +1,10 @@
+export interface Videos {
+  src: string,
+}
+
+export const videosContent: Videos[] = [
+  {src: "https://kaithuerta.com/portfolio/_assets/video/84ead5d58dcfc1894b2cd08c1bbbb731.mp4"},
+  {src: "https://kaithuerta.com/portfolio/_assets/video/6c4d2eae843bc7c47481bf379bc83d86.mp4"},
+  {src: "https://kaithuerta.com/portfolio/_assets/video/18d6573485546c6d66861bfdb61f775e.mp4"},
+  {src: "https://kaithuerta.com/portfolio/_assets/video/eeeac920da87242eb4f1866bb45e7073.mp4"},
+]
