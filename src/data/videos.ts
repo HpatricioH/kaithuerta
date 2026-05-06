c204539a3bdab27e8a28e3549eeea473d5da9103
HPHuerta <hhernanp@gmail.com>
2026-05-05 23:57:48 -0400

    fix: format spacing in videosContent array for consistency

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -5,8 +5,8 @@ export interface Videos {
 }
 
 export const videosContent: Videos[] = [
-  {src: "https://kaithuerta.com/portfolio/_assets/video/84ead5d58dcfc1894b2cd08c1bbbb731.mp4", title: "Ergobaby 360", description: "Parenting/Family Organic"},
-  {src: "https://kaithuerta.com/portfolio/_assets/video/6c4d2eae843bc7c47481bf379bc83d86.mp4", title: "Montessori Knives", description: "Parenting/Family Organic"},
-  {src: "https://kaithuerta.com/portfolio/_assets/video/18d6573485546c6d66861bfdb61f775e.mp4", title: "Montessori Climber", description: "Baby Products Organic"},
-  {src: "https://kaithuerta.com/portfolio/_assets/video/eeeac920da87242eb4f1866bb45e7073.mp4", title: "Mixtiles", description: "Home Decor Organic"},
+  { src: "https://kaithuerta.com/portfolio/_assets/video/84ead5d58dcfc1894b2cd08c1bbbb731.mp4", title: "Ergobaby 360", description: "Parenting/Family Organic" },
+  { src: "https://kaithuerta.com/portfolio/_assets/video/6c4d2eae843bc7c47481bf379bc83d86.mp4", title: "Montessori Knives", description: "Parenting/Family Organic" },
+  { src: "https://kaithuerta.com/portfolio/_assets/video/18d6573485546c6d66861bfdb61f775e.mp4", title: "Montessori Climber", description: "Baby Products Organic" },
+  { src: "https://kaithuerta.com/portfolio/_assets/video/eeeac920da87242eb4f1866bb45e7073.mp4", title: "Mixtiles", description: "Home Decor Organic" },
 ]
